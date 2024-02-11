4991acd4fd86a35c5d52652c62d7e78f4e849432
kihyun940 <houng027@gmail.com>
2024-02-11 18:51:00 +0900

    Feat: 데이터 중심 설계로 기본 매핑 설정 데이터 중심 설계의 단점을 파악하기 위해 기본 매핑 설정 - 테이블 설계에 맞춰서 객체 필드를 구성   - 테이블 외래키를 객체에 그대로 씀 - Order -> private Long memberId; 대신에 -> private Member member;로 객체를 가져오는게 객체 그래프 탐색하기 좋다.
    - @Table(name = "ORDERS") // DB에 ORDER가 예약어로 걸려있을 경우가 있기에 ORDERS로 이름 설정
    
    - private LocalDateTime orderDate; //ORDER_DATE, order_date -> 스프링 부트에서는 자동으로 DB에 카멜케이스를 언더스코어로 바꿔준다.
    
    데이터 중심 설계의 문제점
    • 현재 방식은 객체 설계를 테이블 설계에 맞춘 방식
    • 테이블의 외래키를 객체에 그대로 가져옴
    • 객체 그래프 탐색이 불가능
    • 참조가 없으므로 UML도 잘못됨

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,4 +1,55 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS") // DB에 ORDER가 예약어로 걸려있을 경우가 있기에 ORDERS로 이름 설정
 public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate; //ORDER_DATE, order_date -> 스프링 부트에서는 자동으로 DB에 카멜케이스를 언더스코어로 바꿔준다.
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
