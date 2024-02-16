8e77556258481900dca506e72cfdc88012b811aa
kihyun940 <houng027@gmail.com>
2024-02-17 01:54:13 +0900

    Feat: 연관관계 매핑 시작 - 객체 중심 - Order Member - N : 1   - 다 쪽에 외래키     - 외래키 쪽 : 연관관계 주인     - @ManyToOne 설정     - @JoinColumn(name = "MEMBER_ID") 설정 - 외래키   - 설계 시 단방향부터   - Member - 양방향     - List<Order> orders 추가       - @OneToMany(mappedBy = "member") 설정
    - Order ITEM - N : M 관계
      - OrderItem 추가해서 1 : N  N : 1 관계 설정
    
    - OrderItem
      - Order 쪽 @ManyToOne, @JoinColumn 설정 - 외래키
      - Item 쪽 @ManyToOne, @JoinColumn 설정 - 외래키
    
    - Order OrderItem - 1 : N
      - Order
        - @OneToMany(mappedBy = "order") 설정
        - List<OrderItem> orderItems 추가
    
    ** 핵심은 단방향으로 설계할 수 있으면 단방향으로 설계하자
    - 실무에서 조회를 편하게 하고  JPQL 작성할 때 좀 더 편하게 작성하고 싶을 떄 양방향으로 연관관계 편의 메서드 활용
    
    //연관관계 편의 메서드
        public void addOrderItem(OrderItem orderItem) {
            orderItems.add(orderItem);
            orderItem.setOrder(this);
        }

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,11 +18,21 @@ public class JpaMain {
 
         try {
 
-            //test
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
+            Order order = new Order();
 
-            Member member = em.find(Member.class, memberId);
+//            order.addOrderItem(new OrderItem());
+            /*
+            핵심은 아래처럼 단방향으로 할 수 있으면 단방향으로 설계하자
+            실무에서 조회를 편하게 하고 JPQL 작성할 떄 좀 더 편하게 작성하고 싶을 때 양방향으로 연관관계 편의 메서드 활용
+             */
+
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
 
             tx.commit();
         } catch (Exception e) {
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,6 +3,8 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS") // DB에 ORDER가 예약어로 걸려있을 경우가 있기에 ORDERS로 이름 설정
@@ -13,13 +15,26 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate; //ORDER_DATE, order_date -> 스프링 부트에서는 자동으로 DB에 카멜케이스를 언더스코어로 바꿔준다.
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //연관관계 편의 메서드
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     public Long getId() {
         return id;
@@ -29,12 +44,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
